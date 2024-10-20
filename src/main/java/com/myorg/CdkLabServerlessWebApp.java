c4adc9aa0e1bf2782178db5ee72bd0e487b337d0
codingvn267 <tungthanhnguyen267@gmail.com>
2024-10-20 22:30:30 +0000

    Final  commit for CDK project

--- src/main/java/com/myorg/CdkLabServerlessWebApp.java
+++ src/main/java/com/myorg/CdkLabServerlessWebApp.java
@@ -10,31 +10,20 @@ public class CdkLabServerlessWebApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new CdkLabServerlessWebStack(app, "CdkLabServerlessWebStack", StackProps.builder()
-                // If you don't specify 'env', this stack will be environment-agnostic.
-                // Account/Region-dependent features and context lookups will not work,
-                // but a single synthesized template can be deployed anywhere.
-
-                // Uncomment the next block to specialize this stack for the AWS Account
-                // and Region that are implied by the current CLI configuration.
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
-
-                // Uncomment the next block if you know exactly what Account and Region you
-                // want to deploy the stack to.
-                /*
-                .env(Environment.builder()
-                        .account("123456789012")
-                        .region("us-east-1")
-                        .build())
-                */
-
-                // For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-                .build());
+        CdkLabServerlessWebStack networkStack = new CdkLabServerlessWebStack(app, "CdkLabServerlessWebStack", 
+                StackProps.builder()
+                        .env(Environment.builder()
+                                .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                                .region(System.getenv("CDK_DEFAULT_REGION"))
+                                .build())
+                        .build());
+        new ServerStack(app, "ServerStack", networkStack.getVpc(),
+                StackProps.builder()
+                        .env(Environment.builder()
+                                .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
+                                .region(System.getenv("CDK_DEFAULT_REGION"))
+                                .build())
+                        .build());
 
         app.synth();
     }
--- src/main/java/com/myorg/ServerStack.java
+++ src/main/java/com/myorg/ServerStack.java
@@ -1,48 +1,64 @@
 package com.myorg;
 
+import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.ec2.Instance;
-import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.ec2.MachineImage;
-import software.amazon.awscdk.services.ec2.AmazonLinuxGeneration;
-import software.amazon.awscdk.services.ec2.SecurityGroup;
-import software.amazon.awscdk.services.ec2.Vpc;
-import software.amazon.awscdk.services.ec2.Port;
-import software.amazon.awscdk.services.ec2.SubnetType;
-import software.amazon.awscdk.services.ec2.InstanceClass;
-import software.amazon.awscdk.services.ec2.InstanceSize;
-import software.amazon.awscdk.services.rds.DatabaseInstance;
-import software.amazon.awscdk.services.rds.DatabaseInstanceEngine;
-import software.amazon.awscdk.services.rds.MySqlEngineVersion;
-import software.amazon.awscdk.services.rds.Credentials;
-import software.constructs.Construct;
-
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.rds.*;
 import java.util.Arrays;
 
 public class ServerStack extends Stack {
-  public ServerStack(final Construct scope, final String id, final Vpc vpc) {
-      this(scope, id, vpc, null);
-  }
 
-  public ServerStack(final Construct scope, final String id, final Vpc vpc, final StackProps props) {
-      super(scope, id, props);
+    public ServerStack(final Construct scope, final String id, final Vpc vpc) {
+        this(scope, id, vpc, null);
+    }
 
-      SecurityGroup webServerSG = SecurityGroup.Builder.create(this, "WebServerSG")
-          .vpc(vpc)
-          .allowAllOutBound(true)
-          .build();
-      
-      webServerSG.addIngressRule(Port.tcp(80), "Allow HTTP traffic");
+    public ServerStack(final Construct scope, final String id, final Vpc vpc, final StackProps props) {
+        super(scope, id, props);
 
-      for (int i = 1; i <= 2; i++) {
-          Instance.Builder.create(this, "WebServer" + i)
-                          .vpc(vpc)
-                          .instanceType(InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MEDIUM))  
-                          .machineImage(MachineImage.latestAmazonLinux(AmazonLinuxGeneration.AMAZON_LINUX_2023))  
-                          .securityGroup(webServerSG)  
-                          .build();
-      }
+        
+        SecurityGroup webServerSG = SecurityGroup.Builder.create(this, "WebServerSG")
+                                                        .vpc(vpc)
+                                                        .allowAllOutbound(true)
+                                                        .build();
 
+        
+        webServerSG.addIngressRule(Peer.anyIpv4(), Port.tcp(80), "Allow HTTP traffic");
+
+        
+        for (int i = 1; i <= 2; i++) {
+            Instance.Builder.create(this, "WebServer" + i)
+                            .vpc(vpc)
+                            .instanceType(software.amazon.awscdk.services.ec2.InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO)) 
+                            .machineImage(MachineImage.latestAmazonLinux())
+                            .securityGroup(webServerSG)
+                            .build();
+        }
+
+        
+        SecurityGroup rdsSG = SecurityGroup.Builder.create(this, "RDSSG")
+                                                    .vpc(vpc)
+                                                    .allowAllOutbound(true)
+                                                    .build();
+
+        
+        rdsSG.addIngressRule(webServerSG, Port.tcp(3306), "Allow MySQL traffic from Web Servers");
+
+        
+        DatabaseInstance rdsInstance = DatabaseInstance.Builder.create(this, "RDSInstance")
+                    .engine(DatabaseInstanceEngine.mysql(
+                        MySqlInstanceEngineProps.builder()
+                            .version(MysqlEngineVersion.VER_8_0_26)
+                            .build()
+                    ))
+                    .vpc(vpc)
+                    .vpcSubnets(SubnetSelection.builder()
+                        .subnetType(SubnetType.PRIVATE_WITH_EGRESS)
+                        .build())
+                    .credentials(Credentials.fromGeneratedSecret("admin"))  
+                    .instanceType(software.amazon.awscdk.services.ec2.InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))  
+                    .multiAz(true) 
+                    .securityGroups(Arrays.asList(rdsSG))
+                    .build();
     }
 }
